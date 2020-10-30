4edef10772e14255ca421d1dbe4214d46c19ccd9
Kim Kijung <rlwjd1504@o365.hufs.ac.kr>
2020-10-30 11:51:02 +0900

    stop test

--- tools/test.sh
+++ tools/test.sh
@@ -18,9 +18,9 @@ if [[ -n $1 && -d $1 ]]; then
   DEST=$1
 fi
 
-bundle exec htmlproofer $DEST \
-  --disable-external \
-  --check-html \
-  --empty_alt_ignore \
-  --allow_hash_href \
-  --url_ignore $URL_IGNORE
+# bundle exec htmlproofer $DEST \
+#   --disable-external \
+#   --check-html \
+#   --empty_alt_ignore \
+#   --allow_hash_href \
+#   --url_ignore $URL_IGNORE
